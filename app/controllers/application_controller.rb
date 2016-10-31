95d4d835ce7c9f86e9cd7875566b9afd94c1455b
osamu <noguchi-osamu@av.avex.co.jp>
2016-10-31 11:58:18 +0000

    09_基本課題.md

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,15 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:given_name])
+# <!--追加開始-->    
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:image_url])
+# <!--追加終了-->    
     devise_parameter_sanitizer.permit(:account_update, keys: [:family_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:given_name])
+# <!--追加開始-->   
+    devise_parameter_sanitizer.permit(:account_update, keys: [:image_url])
+# <!--追加終了-->    
+    
   end
   
 
